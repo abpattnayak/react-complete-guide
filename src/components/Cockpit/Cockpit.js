5320756e8e11b4a621d08d0fbd60531c1f3b78db
abpattnayakAptus <abhishek.pattanayak@aptusdatalabs.com>
2019-09-17 18:41:26 +0530

    optimization

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,11 +6,11 @@ const cockpit = (props) => {
     useEffect(()=> {
         console.log('[Cockpit.js] useEffect');
         // http request
-        const timer = setTimeout(() => {
+        setTimeout(() => {
             alert("Saved Data To Cloud!!");
         } , 1000);
         return () => {
-            clearTimeout(timer);
+            // clearTimeout(timer);
             console.log("[Cockpit.js] clean up work in useEffect");
         };
     }, []);
@@ -30,10 +30,10 @@ const cockpit = (props) => {
     if(props.showPersons){
         btnClass = classes.Red;
     }
-    if(props.persons.length <= 2) {
+    if(props.personsLength <= 2) {
       assignedClasses.push(classes.red);
     }
-    if(props.persons.length <= 1) {
+    if(props.personsLength <= 1) {
       assignedClasses.push(classes.bold);
     }
 
@@ -51,4 +51,4 @@ const cockpit = (props) => {
     );
 }
 
-export default cockpit;
+export default React.memo(cockpit);
